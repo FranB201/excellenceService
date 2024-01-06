4b4ddbed01a35da5bd02f51582e9788659c33035
FranB201 <fran.bolufer@hotmail.com>
2024-01-06 13:15:45 +0100

    cambios

--- src/ui/navigation/Navigator.tsx
+++ src/ui/navigation/Navigator.tsx
@@ -1,5 +1,5 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import { LoginScreen } from '../screens/login/login_screen';
+//import { LoginScreen } from '../screens/login/login_screen';
 
 const Stack = createStackNavigator();
 
