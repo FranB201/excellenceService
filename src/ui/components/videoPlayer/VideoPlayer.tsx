037f21beb4f90e7efcfb18e7487aa9ce7679881d
FranB201 <fran.bolufer@hotmail.com>
2024-04-28 20:17:00 +0200

    cambio video

--- src/ui/components/videoPlayer/VideoPlayer.tsx
+++ src/ui/components/videoPlayer/VideoPlayer.tsx
@@ -4,20 +4,22 @@ import { WebView } from 'react-native-webview';
 import { styles } from './VideoPlayerStyle';
 
 
+
 const VideoPlayer = ({ videoId }) => {
-  const vimeoUrl = `https://player.vimeo.com/video/${videoId}?badge=0&autopause=0&player_id=0&app_id=58479`;
+  const vimeoUrl = `https://player.vimeo.com/video/${videoId}?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479`;
 
   return (
     <View style={styles.container}>
       <WebView
-      source={{ uri: vimeoUrl }}
-      allowsInlineMediaPlayback={true}
-      mediaPlaybackRequiresUserAction={false}
-      style={{ width: '100%', height: 300 }} // Asegúrate de tener un alto definido
-      javaScriptEnabled={true}
-      domStorageEnabled={true}
-      startInLoadingState={true} // Muestra un indicador de carga mientras el contenido de la web se está cargando
-      mixedContentMode="compatibility" 
+        source={{ uri: vimeoUrl }}
+        allowsInlineMediaPlayback={true}
+        mediaPlaybackRequiresUserAction={false}
+        style={{ width: '100%', height: 300 }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        mixedContentMode="compatibility"
+        allowsFullscreenVideo={true}  // Asegúrate de permitir video en pantalla completa
       />
     </View>
   );
